03d2d8d07bd62c74a43ef6d711dd9e6537fd286b
Bishuthapa <thapabishu435@gmail.com>
2024-09-02 00:00:14 +0545

    initial commit

---
+++ Data Network and Communcation/all.bat
@@ -0,0 +1,5 @@
+@echo off
+start "chrome" "https://drive.google.com/drive/folders/1381d959A3NGkOm2Cy-qryReccOYP7cs8"
+start "chrome" "https://drive.google.com/drive/folders/1vKid-0jXpEaUH3GVeg0UncR2HPKq80HF"
+start "chrome" "https://drive.google.com/drive/folders/1vpiupZaSv701MOCHv71PVxRT4wl762O3"
+start "chrome" "https://drive.google.com/drive/folders/1vpiupZaSv701MOCHv71PVxRT4wl762O3"
---
+++ Operations research/a.bat
@@ -0,0 +1,9 @@
+@echo off
+start "chrome" "https://www.youtube.com/"
+start "chrome" "D:\Desktop\BIT 4th sem\Operations research\Assignment.pdf"
+start "chrome" "D:\Desktop\BIT 4th sem\Operations research\34-Quantitative-Techniques-for-Business.pdf"
+start "chrome" "D:\Desktop\BIT 4th sem\Operations research\Scanned-Documents.pdf"
+start "chrome" "D:\Desktop\BIT 4th sem\Operations research\ORS255 Operations Research.pdf"
+start "chrome" "D:\Desktop\BIT 4th sem\Operations research\Maximisation-Problem.pdf"
+start "chrome" "https://www.facebook.com/"
+
